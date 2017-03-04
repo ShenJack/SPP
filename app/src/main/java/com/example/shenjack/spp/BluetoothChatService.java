12ab6e2379c6753cef46bde294b0295c9572da6a
ShenJack <357875929@qq.com>
2017-03-04 19:28:39 +0800

    BluetoothChatService

--- app/src/main/java/com/example/shenjack/spp/BluetoothChatService.java
+++ app/src/main/java/com/example/shenjack/spp/BluetoothChatService.java
@@ -4,10 +4,17 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.content.Context;
+import android.os.Bundle;
 import android.os.Handler;
+import android.os.IInterface;
+import android.os.Message;
+import android.provider.SyncStateContract;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * Created by shenjack on 17-3-4.
@@ -31,6 +38,7 @@ public class BluetoothChatService {
     private static final int STATE_LISTEN = 1;
     private static final int STATE_CONNECTTING = 2;
     private static final int STATE_CONNECTED = 3;
+    private int state;
 
     public BluetoothChatService(Context context, Handler handler) {
         mAdapter = BluetoothAdapter.getDefaultAdapter();
@@ -40,12 +48,59 @@ public class BluetoothChatService {
     }
 
 
+    private synchronized void updateUserInterfaceTitle() {
+        mState = getState();
+        mNewState = mState;
+
+        mHandler.obtainMessage(Constants.MESSAGE_STATE_CHANGE, mNewState, -1).sendToTarget();
+    }
+
+    public synchronized int getState() {
+        return state;
+    }
+
+    public synchronized void start(){
+        if(mConnectThread != null){
+            mConnectThread.cancel();
+            mConnectThread = null;
+        }
+
+        if(mConnectedThread != null){
+            mC
+        }
+    }
+
+    private void connectionLost(){
+        Message msg = mHandler.obtainMessage(Constants.MESSAGE_TOAST);
+        Bundle bundle = new Bundle();
+        bundle.putString(Constants.TOAST,"Device connection was lost");
+        msg.setData(bundle);
+        mHandler.sendMessage(msg);
+
+        mState = STATE_NONE;
+
+        updateUserInterfaceTitle();
+
+        BluetoothChatService.this.start();
+    }
+
     private synchronized void connect(BluetoothDevice device,boolean secure){
         if(mState == STATE_CONNECTTING){
             if(mConnectThread != null){
-                mConnectThread.
+                mConnectThread.cancel();
+                mConnectThread = null;
             }
         }
+
+
+        if(mConnectThread!=null){
+            mConnectThread.cancel();
+            mConnectThread=null;
+        }
+
+        mConnectThread = new ConnectThread(device,secure);
+        mConnectThread.start();
+        updateUserInterfaceTitle();
     }
 
     private class ConnectThread extends Thread{
@@ -71,7 +126,75 @@ public class BluetoothChatService {
             mmSocket = tmp;
             mState = STATE_CONNECTTING;
 
+        }
 
+
+
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    private class ConnectedThread extends Thread {
+        private final BluetoothSocket mmSocket;
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
+
+        public ConnectedThread(BluetoothSocket mmSocket,String socketType) {
+            this.mmSocket = mmSocket;
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+
+            try{
+                tmpIn = mmSocket.getInputStream();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+
+            mState = STATE_CONNECTED;
+        }
+
+        public void run(){
+            byte[] buffer = new byte[1024];
+            int bytes;
+
+            while (mState == STATE_CONNECTED){
+                try{
+                    bytes = mmInStream.read(buffer);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    connectionLost();
+                    break;
+                }
+            }
+        }
+
+        public void write(byte[] buffer){
+            try{
+                mmOutStream.write(buffer);
+
+
+                mHandler.obtainMessage(Constants.MESSAGE_WRITE,-1,-1,buffer)
+                        .sendToTarget();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void cancel(){
+            try{
+                mmSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
---
+++ app/src/main/java/com/example/shenjack/spp/Constants.java
@@ -0,0 +1,17 @@
+package com.example.shenjack.spp;
+
+/**
+ * Created by shenjack on 17-3-4.
+ */
+
+public class Constants {
+    public static final int MESSAGE_STATE_CHANGE = 436;
+    public static final int MWSSAGE_READ = 864;
+    public static final int MESSAGE_WRITE = 270;
+    public static final int MESSAGE_DEVICE_NAME = 242;
+    public static final int MESSAGE_TOAST = 872;
+
+    public static final String DEVICE_NAME = "device_name";
+    public static final String TOAST = "toast";
+
+}
