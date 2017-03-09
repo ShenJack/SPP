d6d7793c419c9a5ac31400f619e271f21592e2df
ShenJack <357875929@qq.com>
2017-03-10 00:37:45 +0800

    ALmost done

--- app/src/main/java/com/example/shenjack/spp/MainActivity.java
+++ app/src/main/java/com/example/shenjack/spp/MainActivity.java
@@ -1,40 +1,41 @@
 package com.example.shenjack.spp;
 
+import android.app.ActionBar;
+import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.preference.PreferenceManager;
+import android.content.SharedPreferences;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.ButtonBarLayout;
-import android.support.v7.widget.SwitchCompat;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.ToggleButton;
 
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Set;
-
-import static android.support.v4.app.ActivityCompat.startActivityForResult;
 
 public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
 
     private TextView textView;
     private Set<BluetoothDevice> bondedDevices;
-    private ArrayList<BluetoothDevice> names = new ArrayList<BluetoothDevice>();
 
+    private ArrayAdapter<String> mConversationArrayAdapter;
+    private ListView mConversationView;
 
     private ListView deviceList;
     private EditText mOutEditText;
@@ -42,19 +43,21 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
 
     private BluetoothAdapter mAdapter = BluetoothAdapter.getDefaultAdapter();
     private BluetoothChatService mChatService;
-
+    private String mConnectedDeviceName;
     private StringBuffer mOutStringBuffer;
 
+    private static final int REQUEST_ENABLE_BT = 3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-        registerReceiver(BluetoothReceiver, filter);
-        mSendButton = (Button) findViewById(R.id.send_button
-        );
-        mOutEditText = (EditText) findViewById(R.id.edit_text_out
-        );
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(BluetoothReceiver, filter);
+
+
+        mSendButton = (Button) findViewById(R.id.send_button);
+        mOutEditText = (EditText) findViewById(R.id.edit_text_out);
+        mConversationView = (ListView) findViewById(R.id.in);
 
     }
 
@@ -67,6 +70,17 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
             }
         }
     };
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(!mAdapter.isEnabled()){
+            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableIntent,REQUEST_ENABLE_BT);
+        }else if(mChatService == null){
+            setupChat();
+        }
+    }
 
     @Override
     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -101,6 +115,94 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
         Intent scan = new Intent(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
         sendBroadcast(scan);
     }
+
+    private void setupChat(){
+
+        mConversationArrayAdapter = new ArrayAdapter<String >(this,R.layout.message);
+
+        mConversationView.setAdapter(mConversationArrayAdapter);
+
+        mOutEditText.setOnEditorActionListener(mWriteListener);
+
+        mSendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = mOutEditText.getText().toString();
+                sendMessage(message);
+            }
+        });
+
+        mChatService = new BluetoothChatService(this,mHandler);
+
+        mOutStringBuffer = new StringBuffer("");
+    }
+
+    private void setStatus(int resId){
+        final ActionBar actionBar = this.getActionBar();
+        if(null == actionBar){
+            return;
+        }
+        actionBar.setSubtitle(resId);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if(mChatService!=null){
+            if(mChatService.getState() == BluetoothChatService.STATE_NONE){
+                mChatService.start();
+            }
+        }
+    }
+
+    private void setStatus(CharSequence subTitle) {
+        Activity activity = this;
+        final ActionBar actionBar = activity.getActionBar();
+        if (null == actionBar) {
+            return;
+        }
+        actionBar.setSubtitle(subTitle);
+    }
+
+
+    private final Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case Constants.MESSAGE_STATE_CHANGE:
+                    switch (msg.arg1) {
+                        case BluetoothChatService.STATE_CONNECTED:
+                            setStatus("Connected to" + mConnectedDeviceName);
+                            mConversationArrayAdapter.clear();
+                            break;
+                        case BluetoothChatService.STATE_CONNECTTING:
+                            setStatus("connecting");
+                            break;
+                        case BluetoothChatService.STATE_LISTEN:
+                        case BluetoothChatService.STATE_NONE:
+                            setStatus("not connected");
+                            break;
+                    }
+                    break;
+                case Constants.MESSAGE_WRITE:
+                    byte[] writeBuf = (byte[]) msg.obj;
+                    String writeMessage = new String(writeBuf);
+                    mConversationArrayAdapter.add("Me:  " + writeMessage);
+                    break;
+                case Constants.MWSSAGE_READ:
+                    byte[] readBuf = (byte[]) msg.obj;
+                    String readMessage = new String(readBuf, 0, msg.arg1);
+                    mConversationArrayAdapter.add(mConnectedDeviceName + ": " + readMessage);
+                    break;
+                case Constants.MESSAGE_DEVICE_NAME:
+                    mConnectedDeviceName = msg.getData().getString(Constants.DEVICE_NAME);
+                    Toast.makeText(MainActivity.this, "Connected to "+mConnectedDeviceName,
+                            Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        }
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -156,9 +258,24 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
         }
     }
 
+    private TextView.OnEditorActionListener mWriteListener
+            = new TextView.OnEditorActionListener() {
+        @Override
+        public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+            if(actionId == EditorInfo.IME_NULL && event.getAction() == KeyEvent.ACTION_UP){
+                String message = v.getText().toString();
+                sendMessage(message);
+            }
+            return true;
+        }
+    };
+
     @Override
     protected void onDestroy() {
         unregisterReceiver(BluetoothReceiver);
+        if(mChatService != null){
+            mChatService.stop();
+        }
         super.onDestroy();
     }
 }
